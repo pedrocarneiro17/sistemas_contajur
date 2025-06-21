641e82ce24e04af85e1425002da7b59c641eff48
Pedro Carneiro <pedro.aco@ufop.aluno.edu.br>
2025-06-21 19:33:06 -0300

    ajuste main 2

--- home/main.py
+++ home/main.py
@@ -7,4 +7,5 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from extrator_contajur import app  # Importa o app definido em app.py
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+

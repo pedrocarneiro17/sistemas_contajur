b2bec1d127c72cd0525d4850a742356e1f47b9e9
Pedro Carneiro <pedro.aco@ufop.aluno.edu.br>
2025-06-21 19:27:31 -0300

    ajuste main

--- home/main.py
+++ home/main.py
@@ -1,8 +1,10 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+# Garante que o diretório pai (que contém extrator_contajur) esteja no sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from extrator_contajur import app  # Importa o app definido em app.py
 
 if __name__ == '__main__':
-    app.run(debug=False)  # Remova host e port, pois o Vercel gerencia isso
+    app.run()

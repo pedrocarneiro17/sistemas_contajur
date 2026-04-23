3f0ef597f953e3ad1ec13e6785810ee04e8c8d62
Pedro Carneiro <pedro.aco@ufop.aluno.edu.br>
2026-04-23 09:12:10 -0300

    adaptação santander

--- extrator_contajur/banco/santander1.py
+++ extrator_contajur/banco/santander1.py
@@ -15,7 +15,7 @@ def preprocess_text(text):
         "Aplicativo Santander", "Internet Banking Empresarial",
         "Agência:", "Conta:", "Período", "Data/Hora:", "Saldo disponível",
         "Saldo de ContaMax", "Entenda a composição", "Central de Atendimento",
-        "SAC", "Ouvidoria", "Data Histórico", "Saldo do dia",
+        "SAC", "Ouvidoria", "Data Histórico", "Saldo do dia", "Saldo anterior",
         "4004-2125", "0800",
     }
 
